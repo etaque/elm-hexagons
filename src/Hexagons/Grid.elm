0a25b91b50f2b1f91f3bd255f3dc3ac7b61bb805
Emilien Taque <etaque@gmail.com>
2016-11-16 22:41:47 +0100

    0.18 upgrade

--- src/Hexagons/Grid.elm
+++ src/Hexagons/Grid.elm
@@ -12,74 +12,84 @@ module Hexagons.Grid exposing (Grid, Row, Tile, get, set, delete, list, getPoint
 @docs set, delete
 -}
 
-
 import Dict exposing (Dict)
 import Hexagons exposing (..)
 
 
-{-| A grid is a Dict of rows -}
-type alias Grid a = Dict Int (Row a)
+{-| A grid is a Dict of rows
+-}
+type alias Grid a =
+    Dict Int (Row a)
 
 
-{-| A row is a Dict of values -}
-type alias Row a = Dict Int a
+{-| A row is a Dict of values
+-}
+type alias Row a =
+    Dict Int a
 
 
-{-| A tile has coords and some content -}
+{-| A tile has coords and some content
+-}
 type alias Tile a =
-  { content : a
-  , coords : Axial
-  }
+    { content : a
+    , coords : Axial
+    }
 
 
-{-| Find eventual content in grid on those axial coordinates -}
+{-| Find eventual content in grid on those axial coordinates
+-}
 get : Grid a -> Axial -> Maybe a
-get grid (i, j) =
-  (Dict.get i grid) `Maybe.andThen` (Dict.get j)
+get grid ( i, j ) =
+    (Dict.get i grid) |> Maybe.andThen (Dict.get j)
 
 
-{-| Add or update content to grid on those axial coordinates -}
+{-| Add or update content to grid on those axial coordinates
+-}
 set : a -> Axial -> Grid a -> Grid a
-set tile (i,j) grid =
-  let
-    updateRow maybeRow =
-      case maybeRow of
-        Just row ->
-          Dict.insert j tile row
-        Nothing ->
-          Dict.singleton j tile
-  in
-    Dict.insert i (updateRow (Dict.get i grid)) grid
+set tile ( i, j ) grid =
+    let
+        updateRow maybeRow =
+            case maybeRow of
+                Just row ->
+                    Dict.insert j tile row
+
+                Nothing ->
+                    Dict.singleton j tile
+    in
+        Dict.insert i (updateRow (Dict.get i grid)) grid
 
 
-{-| Remove content from those axial coordinates within grid -}
+{-| Remove content from those axial coordinates within grid
+-}
 delete : Axial -> Grid a -> Grid a
-delete (i, j) grid =
-  let
-    deleteInRow maybeRow =
-      case maybeRow of
-        Just row ->
-          Dict.remove j row
-        Nothing ->
-          Dict.empty
-  in
-    Dict.insert i (deleteInRow (Dict.get i grid)) grid
+delete ( i, j ) grid =
+    let
+        deleteInRow maybeRow =
+            case maybeRow of
+                Just row ->
+                    Dict.remove j row
+
+                Nothing ->
+                    Dict.empty
+    in
+        Dict.insert i (deleteInRow (Dict.get i grid)) grid
 
 
-{-| Produce a list of all tiles within grid -}
+{-| Produce a list of all tiles within grid
+-}
 list : Grid a -> List (Tile a)
 list grid =
-  let
-    rows =
-      Dict.toList grid
+    let
+        rows =
+            Dict.toList grid
 
-    mapRow (i, row) =
-      List.map (mapTile i) (Dict.toList row)
+        mapRow ( i, row ) =
+            List.map (mapTile i) (Dict.toList row)
 
-    mapTile i (j, kind) =
-      Tile kind (i, j)
-  in
-    List.concatMap mapRow rows
+        mapTile i ( j, kind ) =
+            Tile kind ( i, j )
+    in
+        List.concatMap mapRow rows
 
 
 {-| Given an hexagonal grid definition (radius and grid),
@@ -87,5 +97,5 @@ what's in the hexagon holding this point?
 -}
 getPoint : Float -> Grid a -> Point -> Maybe a
 getPoint hexRadius grid p =
-  Hexagons.pointToAxial hexRadius p
-    |> get grid
+    Hexagons.pointToAxial hexRadius p
+        |> get grid
